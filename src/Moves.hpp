c8caf9977c4802ea663473746bb6efa3ec6bfff3
Jonathan Evans <JonathanEvansDev@gmail.com>
2025-03-06 14:12:33 -0600

    Update move to accomodate different move types

--- src/Moves.hpp
+++ src/Moves.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+#include <variant>
+
+#include "Tile.hpp"
+#include "Word.hpp"
+
 namespace Moves {
-class Move {};
+
+class Pass {};
+
+struct SwapTile {
+  Tile inTile;
+  Tile outTile;
+};
+
+struct PlaceWord {
+  enum class WordOwner { ME, THEM };
+
+  Word word;
+  WordOwner owner;
+};
+
+using Move = std::variant<Pass, SwapTile, PlaceWord>;
+
 }; // namespace Moves
